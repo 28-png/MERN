ad7db750d1ee2589c5b39929e26c271ce2ea1864
Matt Murphy <murphy.d.matthew28@gmail.com>
2021-01-18 12:23:42 -0600

    updated color scheme

--- client1/src/styles.js
+++ client1/src/styles.js
@@ -2,7 +2,6 @@ import { makeStyles } from '@material-ui/core/styles';
 
 export default makeStyles((theme) => ({
     appBar: {
-        backgroundColor: 'rgba(0,183,255, 1)',
         borderRadius: 15,
         margin: '30px 0',
         display: 'flex',
