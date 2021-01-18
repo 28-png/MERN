05c2b489a37f2c92b39f4ed005fb14f629568528
Matt Murphy <murphy.d.matthew28@gmail.com>
2021-01-17 18:19:12 -0600

    updated header icon

--- client1/src/App.js
+++ client1/src/App.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { Container, AppBar, Typography, Grow, Grid } from '@material-ui/core';
-import memories from './images/memories.png';
+import Gamer from './images/Gamer.png';
 import Posts from './components/Posts/Posts';
 import Form from './components/Form/Form';
 import useStyles from './styles';
@@ -20,9 +20,9 @@ const App = () => {
      <Container maxidth="lg">
          <AppBar className={classes.appBar} position="static" color="inherit">
             <Typography className={classes.heading} variant="h2" align="center">
-                Memories
+                Hello Gamer 
             </Typography>
-            <img className={classes.image} src={memories} alt="memories" height="60" />
+            <img className={classes.image} src={Gamer} alt="Gamer" height="60" />
          </AppBar>
             <Grow in>
                 <Container>
