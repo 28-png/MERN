a67d8d3f9d53567878364b6050ddf2b7481954f5
Matt Murphy <murphy.d.matthew28@gmail.com>
2021-01-17 18:39:15 -0600

    updated

--- client1/src/components/Form/styles.js
+++ client1/src/components/Form/styles.js
@@ -1,13 +1,16 @@
 import { makeStyles } from '@material-ui/core/styles';
 
+
 export default makeStyles((theme) => ({
   root: {
     '& .MuiTextField-root': {
       margin: theme.spacing(1),
+      // backgroundColor: 'white'
     },
   },
   paper: {
     padding: theme.spacing(2),
+    // backgroundColor: 'rgba(0,183,255, 1)',
   },
   form: {
     display: 'flex',
--- client1/src/styles.js
+++ client1/src/styles.js
@@ -2,6 +2,7 @@ import { makeStyles } from '@material-ui/core/styles';
 
 export default makeStyles((theme) => ({
     appBar: {
+        backgroundColor: 'rgba(0,183,255, 1)',
         borderRadius: 15,
         margin: '30px 0',
         display: 'flex',
